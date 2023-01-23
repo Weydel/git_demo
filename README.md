ff90cfeb05cc02cd2dd3bec3c7da51e4357c329e
Waheed Tijani <twaheed2005@gmail.com>
2023-01-23 13:47:36 +0000

    Form

--- README.md
+++
@@ -1,4 +0,0 @@
-# git_demo
-#This is  a demo code to demonstrate pushing a file from VSCode to Github
-
-#File was created in VSCode and pushed to Github
--- demo.py
+++
@@ -1,6 +0,0 @@
-print("Hello World!!")
-print("Hello iNeuron!!")
-print("Hello World!!")
-print("Hello iNeuron!!")
-print("This is Line 5 updated in Github")
-print("Hello World!!")
