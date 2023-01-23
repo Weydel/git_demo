cd9ffa24a502d186402b054b242b115d103f5391
Waheed Tijani <twaheed2005@gmail.com>
2023-01-23 12:29:26 +0000

    New Commit

--- demo.py
+++ demo.py
@@ -4,8 +4,3 @@ print("Hello World!!")
 print("Hello iNeuron!!")
 print("This is Line 5 updated in Github")
 print("Hello World!!")
-<<<<<<< HEAD
-print("Hello iNeuron!!")
-=======
-print("Hello iNeuron!!")
->>>>>>> df915ddcfcfcb74e51ce187f6103ece21e0198d5
